f21479935c7ae8a3cf753dc131ef55f9c1d2f84a
michaelsbday <89911195+michaelsbday@users.noreply.github.com>
2021-09-01 22:05:56 +0700

    Update script-welcome.js

--- script-welcome.js
+++ script-welcome.js
@@ -2,9 +2,8 @@ function validate() {
 	var value = document.getElementById("password-entry").value;
     console.log(value);
 	if (value == "hon") {
-        window.open("https://michaelsbday.github.io/gallery.html");
-        document.getElementById("password-entry").innerHTML = "";
-
+		window.open("https://michaelsbday.github.io/gallery.html");
+		document.getElementById("password-entry").innerHTML = "";
 	} else {
         alert("salah");
 		return false;
