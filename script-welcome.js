e5cf6196c3006609c2121af5b98ca8ca2ea22c0e
Jessica Aurellia <jessrellia@gmail.com>
2021-09-04 15:36:21 +0700

    update link

--- script-welcome.js
+++ script-welcome.js
@@ -1,7 +1,7 @@
 function validate() {
 	var value = document.getElementById("password-entry").value;
     console.log(value);
-	if (value == "hon") {
+	if (value == "143ILY0oct747084") {
 		window.open("https://michaelsbday.github.io/gallery.html");
 		document.getElementById("password-entry").innerHTML = "";
 	} else {
